4ead5b4e18a8722715af6b2a8d750b5386aaf185
AlexanderHub99 <agames1448@gmail.com>
2021-09-12 20:20:24 +0300

    Add Web site

---
+++ What_can_I_do_/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace What_can_I_do_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
